290f74e94d45f575ed954e2b4598b8777ee8f519
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2022-12-31 17:02:50 -0500

    Update Atack.sh

--- Atack.sh
+++ Atack.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+
 clear
 setterm -foreground red
 echo "
@@ -34,8 +35,39 @@ echo -n "[•]Opcion > "
 read opcion
 case $opcion in
 1)
-clear
 #! /bin/bash
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+
+
+#directory verification
+directory=$(pwd)
+
+
+if which figlet >/dev/null; then
+sleep 1
+echo ""
+echo -e "$blue(figlet)$nc ................................................... Instalado [$green✓$nc]"
+else
+sleep 1
+echo -e "$red(figlet)$nc No instalado [$red✗$nc]"
+sleep 1
+echo -e "\e[1;32mInstalando figlet ...\e[0m"
+sleep 3
+pkg install figlet
+fi
+clear
+
 setterm -foreground red
 echo "
            ⠀      ⢀⣾⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
